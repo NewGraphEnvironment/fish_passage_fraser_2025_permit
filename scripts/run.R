fad640363454cef3a35d023f287919a22893fb82
almac2022 <al@newgraphenvironment.com>
2025-07-28 00:24:49 -0700

    Paramatize memo name to avoid errors related to dfo vs moe only memos as per https://github.com/NewGraphEnvironment/fish_passage_peace_2025_permit/issues/1

--- scripts/run.R
+++ scripts/run.R
@@ -2,23 +2,56 @@
 # just run this if we want latest util function.  Really just an example here
 staticimports::import()
 
+# {
+#   rmarkdown::render(input = 'application_moe.Rmd', output_file = 'docs/index.html')
+#   pagedown::chrome_print(input = 'application_moe.Rmd', output = 'docs/application_moe_dfo.pdf')
+#
+#   # we want to separately attach a map and a list of sites for DFO
+#   #
+#   ## trim up the file.  We ditch the last page only when there are references.
+#
+#   # specifiy the page numbers of the map and the site lists
+#   page_map <- 5
+#   page_site_list <- 6:12
+#
+#   pdftools::pdf_subset("docs/application_moe_dfo.pdf",
+#                        pages = page_map, output = "docs/application_dfo_map.pdf")
+#
+#   # trim up the file.  We ditch the last page only when there are references.
+#   pdftools::pdf_subset("docs/application_moe_dfo.pdf",
+#                        pages = page_site_list, output =  "docs/application_dfo_site_list.pdf")
+# }
+
 {
-  rmarkdown::render(input = 'application_moe.Rmd', output_file = 'docs/index.html')
-  pagedown::chrome_print(input = 'application_moe.Rmd', output = 'docs/application_moe_dfo.pdf')
+  # Parameterize the memo name
+  name_memo <- "application_moe"
 
-  # we want to separately attach a map and a list of sites for DFO
-  #
-  ## trim up the file.  We ditch the last page only when there are references.
+  # Render outputs
+  rmarkdown::render(
+    input = paste0(name_memo, ".Rmd"),
+    output_file = fs::path("docs", "index.html")
+  )
 
-  # specifiy the page numbers of the map and the site lists
+  pagedown::chrome_print(
+    input = paste0(name_memo, ".Rmd"),
+    output = fs::path("docs", paste0(name_memo, ".pdf"))
+  )
+
+  # DFO-specific attachments: trim the map and site list pages
   page_map <- 5
   page_site_list <- 6:12
 
-  pdftools::pdf_subset("docs/application_moe_dfo.pdf",
-                       pages = page_map, output = "docs/application_dfo_map.pdf")
+  input_pdf <- fs::path("docs", paste0(name_memo, ".pdf"))
 
-  # trim up the file.  We ditch the last page only when there are references.
-  pdftools::pdf_subset("docs/application_moe_dfo.pdf",
-                       pages = page_site_list, output =  "docs/application_dfo_site_list.pdf")
+  pdftools::pdf_subset(
+    input = input_pdf,
+    pages = page_map,
+    output = fs::path("docs", "application_dfo_map.pdf")
+  )
+
+  pdftools::pdf_subset(
+    input = input_pdf,
+    pages = page_site_list,
+    output = fs::path("docs", "application_dfo_site_list.pdf")
+  )
 }
-
